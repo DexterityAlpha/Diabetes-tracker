26cfd74483066b09fa4443ce9a4ce9948193d0db
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2021-06-25 23:35:17 +0530

    Login Page UI created

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  
   static const routename = "/loginscreen";
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FittedBox(
+            child: Image.asset("assets/images/splash2.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).buttonColor),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(16))),
+                  onPressed: () {},
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.headline6!.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
